6bd233a1c242e3357facb514237c5d0e0b705a63
Raul <rauldavila323@gmail.com>
2021-02-03 10:54:50 +0100

    Buttons have been given animation and corresponding sound effect.

--- game.js
+++ game.js
@@ -13,12 +13,18 @@ function nextSequence(){
 
 	let playSound = new Audio("sounds/" + randomChosenColor + '.mp3')
 	playSound.play()
-}
 
+}
 
 
    $('.btn').click(function(){
 		let userChosenColour = $(this).attr('id') ;
 		userClickedPattern.push(userChosenColour);
+
+		 $("#" + userChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+		 let playSound = new Audio("sounds/" + userChosenColour + '.mp3')
+		 playSound.play()
    });
+
 
